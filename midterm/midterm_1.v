4048d7e50cfb7dacf3b6ac993288c6802749049a
ChinlinWu <tj.c@nycu.edu.tw>
2022-11-07 11:41:51 +0800

    version 1 completed

--- midterm/midterm_1.v
+++ midterm/midterm_1.v
@@ -1,3 +1,10 @@
+// A file that can show ABC...on row_A and row_B, 
+// while after pressing any buttons, they start shifting 
+// one position every 1 second, where row_A goes left, row_B goes right.
+// Pressing any buttons again can stop the shifting, then again to start
+// shifting, and so on.
+
+
 `timescale 1ns / 1ps
 
 module midterm(
@@ -14,7 +21,7 @@ module midterm(
     // turn off all the LEDs
     assign usr_led = 4'b0000;
  
-
+    // Setting up the four buttons
     wire btn_level_0, btn_pressed_0;
     reg prev_btn_level_0;    
     wire btn_level_1, btn_pressed_1;
@@ -25,19 +32,20 @@ module midterm(
     reg prev_btn_level_3;
 
     always @(posedge clk) begin
-        if (~reset_n)
+        if (~reset_n) begin
             prev_btn_level_0 <= 1;
             prev_btn_level_1 <= 1;
             prev_btn_level_2 <= 1;
             prev_btn_level_3 <= 1;
-        else
+        end
+        else begin
             prev_btn_level_0 <= btn_level_0;
             prev_btn_level_1 <= btn_level_1;
             prev_btn_level_2 <= btn_level_2;
             prev_btn_level_3 <= btn_level_3;
+        end
     end
  
-
     debounce btn_db0(
         .clk(clk),
         .btn_input(usr_btn[0]),
@@ -59,28 +67,28 @@ module midterm(
         .btn_output(btn_level_3)
     );
 
+    assign btn_pressed_0 = (btn_level_0 == 1 && prev_btn_level_0 == 0);
+    assign btn_pressed_1 = (btn_level_1 == 1 && prev_btn_level_1 == 0);
+    assign btn_pressed_2 = (btn_level_2 == 1 && prev_btn_level_2 == 0);
+    assign btn_pressed_3 = (btn_level_3 == 1 && prev_btn_level_3 == 0);
+    // End setting up the four buttons
 
-    reg rev = 0;
+
+
+    reg [26:0] counter = 0;
     reg launched = 0;  // Whether entered into the demo, or stuck in the welcome message
-    reg [127:0] alphabet = "ABCDEFGHIJKLMNOP";
-    reg [4:0] current_alphabet;
+    reg [4:0]idx = 0;
+    //reg [127:0] alphabet = "ABCDEFGHIJKLMNOP";
+    //reg [4:0] current_alphabet;
     
     reg [127:0] row_A = "ABCDEFGHIJKLMNOP";
-    reg [127:0] row_B = "QRSTUVWXYZABCDEF"
+    reg [127:0] row_B = "QRSTUVWXYZABCDEF";
      
      
     //-----------------------------------
     //shifting module
     //-----------------------------------
-    for(idx = 0; idx<16; idx = idx+1 ) begin 
-        if (alphabet[idx*8 +: 8] >= "Z") alphabet[idx*8 +: 8] <= "A"; 
-        else alphabet[idx*8 +: 8] <= alphabet[idx*8 +: 8] + 1;
-    end
 
-    for(idx = 0; idx<16; idx = idx+1 ) begin 
-        if (alphabet[idx*8 +: 8] <= "A") alphabet[idx*8 +: 8] <= "Z"; 
-        else alphabet[idx*8 +: 8] <= alphabet[idx*8 +: 8] - 1;
-    end
 
     LCD_module lcd0(
         .clk(clk),
@@ -92,24 +100,45 @@ module midterm(
         .LCD_RW(LCD_RW),
         .LCD_D(LCD_D)
     );
-
-
-    assign btn_pressed = (btn_level == 1 && prev_btn_level == 0);
-
- 
-    
-
  
     // Upon are given in the sample code
  
 
     always @(posedge clk) begin
         if (~reset_n) begin
-
+            launched <= 0;
+            row_A <= "ABCDEFGHIJKLMNOP";
+            row_B <= "QRSTUVWXYZABCDEF";
         end
  
         else if (launched) begin
-            
+
+            if (btn_pressed_0) launched <= 0;
+            if (btn_pressed_1) launched <= 0;
+            if (btn_pressed_2) launched <= 0;
+            if (btn_pressed_3) launched <= 0;
+
+            if (counter < 100000000) begin
+                counter <= counter + 1;
+            end
+            else begin
+                counter<=0;
+                for(idx = 0; idx<16; idx = idx+1 ) begin 
+                    if (row_A[idx*8 +: 8] >= "Z") row_A[idx*8 +: 8] <= "A"; 
+                    else row_A[idx*8 +: 8] <= row_A[idx*8 +: 8] + 1;
+                end
+
+                for(idx = 0; idx<16; idx = idx+1 ) begin 
+                    if (row_B[idx*8 +: 8] <= "A") row_B[idx*8 +: 8] <= "Z"; 
+                    else row_B[idx*8 +: 8] <= row_B[idx*8 +: 8] - 1;
+                end
+            end
+        end
+        else if(~launched) begin
+            if (btn_pressed_0) launched <= 1;
+            if (btn_pressed_1) launched <= 1;
+            if (btn_pressed_2) launched <= 1;
+            if (btn_pressed_3) launched <= 1;
         end
     end
  
