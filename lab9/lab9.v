b95703228deade6d87124c3be3d81c13413d9bb7
Chinlin <57287199+tjwu570@users.noreply.github.com>
2022-12-02 21:24:46 +0800

    1202_md5_2124

--- lab9/lab9.v
+++ lab9/lab9.v
@@ -1,34 +1,5 @@
 `timescale 1ps/1ns
 
-reg [0 : 127] passwd_hash = 128'hE8CD0953ABDFDE433DFEC7FAA70DF7F6;
-
-reg [95 : 0] cnt = 0;
-
-reg [4 : 0] r [0 : 5] = {
-7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 
-5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 
-4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 
-6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21 };
-
-reg [31 : 0] k [0 : 5] = {
-32'hd76aa478, 32'he8c7b756, 32'h242070db, 32'hc1bdceee, 
-32'hf57c0faf, 32'h4787c62a, 32'ha8304613, 32'hfd469501, 
-32'h698098d8, 32'h8b44f7af, 32'hffff5bb1, 32'h895cd7be, 
-32'h6b901122, 32'hfd987193, 32'ha679438e, 32'h49b40821, 
-32'hf61e2562, 32'hc040b340, 32'h265e5a51, 32'he9b6c7aa, 
-32'hd62f105d, 32'h02441453, 32'hd8a1e681, 32'he7d3fbc8, 
-32'h21e1cde6, 32'hc33707d6, 32'hf4d50d87, 32'h455a14ed, 
-32'ha9e3e905, 32'hfcefa3f8, 32'h676f02d9, 32'h8d2a4c8a, 
-32'hfffa3942, 32'h8771f681, 32'h6d9d6122, 32'hfde5380c, 
-32'ha4beea44, 32'h4bdecfa9, 32'hf6bb4b60, 32'hbebfbc70, 
-32'h289b7ec6, 32'heaa127fa, 32'hd4ef3085, 32'h04881d05, 
-32'hd9d4d039, 32'he6db99e5, 32'h1fa27cf8, 32'hc4ac5665, 
-32'hf4292244, 32'h432aff97, 32'hab9423a7, 32'hfc93a039, 
-32'h655b59c3, 32'h8f0ccc92, 32'hffeff47d, 32'h85845dd1, 
-32'h6fa87e4f, 32'hfe2ce6e0, 32'ha3014314, 32'h4e0811a1, 
-32'hf7537e82, 32'hbd3af235, 32'h2ad7d2bb, 32'heb86d391};
- 
-
 module lab9(
   // General system I/O ports
   input  clk,
@@ -42,11 +13,9 @@ module lab9(
   output [3:0] LCD_D
 );
 
-reg [127 : 0] hash_0 = 0;
 
-md5_0000_0000 md5_0(
-    hash_outcome(hash_0)
-)
+reg [0 : 127] passwd_hash = 128'hE8CD0953ABDFDE433DFEC7FAA70DF7F6;
+
 
 wire btn_level, btn_pressed;
 reg  prev_btn_level;
@@ -68,6 +37,7 @@ LCD_module lcd0(
     .LCD_D(LCD_D));
 
 // Timer
+reg [95 : 0] cnt = 0;
 always @(posedge clk) begin
     if (P == S_MAIN_INIT)
         cnt <= "000000000000";
@@ -116,12 +86,3 @@ end
  endmodule
 
 
-
- module md5_0000_0000(output reg [127 : 0] hash_outcome)
-
- reg [31 : 0] h0 = 32'h67452301;
- reg [31 : 0] h1 = 32'h67452301;
- reg [31 : 0] h2 = 32'h67452301;
- reg [31 : 0] h3 = 32'h67452301;
-
- endmodule
